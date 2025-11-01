c020ececeedf1803aaf6797c3e4a42d0a5265fc0
aceny113 <jasonyance13@gmail.com>
2025-11-02 02:57:48 +0800

    can now change background of box when hovering but can't change the gridSize

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+let gridSize = 16;
 
-function makeGrid(gridSize) {
-  const table = document.querySelector(".table");
-  for (let i = 1; i <= gridSize; i++) {
-    let cols = document.createElement("div");
-    cols.className = "cols";
+const table = document.querySelector(".table");
+for (let i = 1; i <= gridSize; i++) {
+  let cols = document.createElement("div");
+  cols.className = "cols";
 
-    for (let j = 1; j <= gridSize; j++) {
-      let row = document.createElement("div");
-      row.className = "box row";
-      cols.appendChild(row);
-    }
-
-    table.appendChild(cols);
+  for (let j = 1; j <= gridSize; j++) {
+    let row = document.createElement("div");
+    row.className = "box row";
+    cols.appendChild(row);
   }
+
+  table.appendChild(cols);
 }
-makeGrid(16);
+
+
+for(i = 0; i < document.querySelectorAll(".box").length; i++) {
+  let row = document.querySelectorAll(".box")[i];
+row.addEventListener("mouseover", (event) => {
+  row.classList.add("change-bg");
+});
+}
+
+function askUserGridSize() {
+  gridSize = Number(prompt("Enter Grid Size You Want. Minimum of 1 and Maximum of 100"));
+} 
