92e7b74e9bcc880c6c8c5105547e2bbc4f6dd9c4
aceny113 <jasonyance13@gmail.com>
2025-12-21 22:49:48 +0800

    add randomize color feature

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//make grid
 function grid(gridSize = 16) {
   const table = document.querySelector(".table");
   for (let i = 1; i <= gridSize; i++) {
@@ -28,18 +30,33 @@ function createRow() {
 
 function changeBg() {
   const rowsList = document.querySelectorAll(".box");
+  const colorChoice = document.querySelector("#color-choice");
+  
   for (let i = 0; i < rowsList.length; i++) {
     const rows = rowsList[i];
-    rows.addEventListener("mouseover", () => {
-      rows.classList.add("change-bg");
+    rows.addEventListener("mouseenter", () => {
+      
+      // rows.classList.add("change-bg");
+      // rows.style.backgroundColor = colorChoice.value;
+      rows.style.backgroundColor = randomColor();
     });
   }
+  
 }
+
+function randomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
 
 function askNewGrid() {
   const newGrid = Number(prompt("Enter new Grid. 1 - 100"));
   const table = document.querySelector(".table");
 
+  //remove rows and columns
   const colsList = document.querySelectorAll(".cols");
   for (let i = 0; i < colsList.length; i++) {
     const cols = colsList[i];
